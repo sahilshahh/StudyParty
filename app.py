72f7f01311cd51f90059fac39c5c1396ed3948f9
jihwan923 <jxl131130@utexas.edu>
2017-04-20 12:33:22 -0500

    click scalability

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from flask_heroku import Heroku
 from helper import add_data, fetch_data
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/studyparty'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:321884@localhost:5432/studyparty'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/studyparty'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:321884@localhost:5432/studyparty'
 #heroku = Heroku(app)
 db = SQLAlchemy(app)
 
@@ -15,13 +15,19 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    x_coordinate = db.Column(db.String(120), unique=True) 
-    y_coordinate = db.Column(db.String(120), unique=True)
+    x_coordinate = db.Column(db.Float(), unique=True) 
+    y_coordinate = db.Column(db.Float(), unique=True)
+    screen_x = db.Column(db.Float(), unique=False)
+    screen_y = db.Column(db.Float(), unique=False)
+    zoomLevel = db.Column(db.String(120), unique=False)
     time = db.Column(db.DateTime, unique=True)
 
-    def __init__(self, x_coordinate, y_coordinate, time):
+    def __init__(self, x_coordinate, y_coordinate,screen_x, screen_y, zoomLevel, time):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
+        self.screen_x = screen_x
+        self.screen_y = screen_y
+        self.zoomLevel = zoomLevel
         self.time = time
 
     def __repr__(self):
@@ -30,7 +36,7 @@ class User(db.Model):
 # Set "homepage" to index.html and fetches data for the heatmap
 @app.route('/')
 def index():
-    x_coord, y_coord, length = fetch_data(db, placeholder = None)
+    x_coord, y_coord, screen_xs, screen_ys, zoomLevels, length = fetch_data(db, placeholder = None)
     #x_coord = []
     #y_coord = []
     #users = db.session.query(User)
@@ -47,28 +53,28 @@ def index():
             #y_coord.append(int(float(user.y_coordinate)))
             #length+=1
     #length = int(length)
-    return render_template('demo.html', x_coord=x_coord, y_coord=y_coord, length=length)
+    return render_template('demo.html', x_coord=x_coord, y_coord=y_coord, screen_xs=screen_xs, screen_ys=screen_ys, zoomLevels=zoomLevels, length=length)
 
 # Adds info to database and redirects to function that creats link
 @app.route('/prereg', methods=['GET','POST'])
 def prereg():
     if request.method == 'POST':
-        x_coordinate, y_coordinate, placeholder = add_data(db, request.form)
+        x_coordinate, y_coordinate, screen_x, screen_y, zoomLevel, placeholder = add_data(db, request.form)
         #x_coordinate = request.form['leftPixel']
         #y_coordinate = request.form['topPixel']
         #time = datetime.datetime.now()
         #reg = User(x_coordinate, y_coordinate, time)
         #db.session.add(reg)
         #db.session.commit()
-        return redirect(url_for('map', x_coordinate=x_coordinate, y_coordinate=y_coordinate))
+        return redirect(url_for('map', x_coordinate=x_coordinate, y_coordinate=y_coordinate, screen_x=screen_x, screen_y=screen_y, zoomLevel=zoomLevel))
     else:
         return redirect(url_for('index'))
 
 #generates the link for our map
-@app.route('/map/<x_coordinate>/<y_coordinate>', methods=['GET','POST'])
-def map(x_coordinate, y_coordinate):
+@app.route('/map/<x_coordinate>/<y_coordinate>/<screen_x>/<screen_y>/<zoomLevel>', methods=['GET','POST'])
+def map(x_coordinate, y_coordinate, screen_x, screen_y, zoomLevel):
     if request.method == 'GET':
-        return render_template('democlick.html', x_coordinate=x_coordinate, y_coordinate=y_coordinate)
+        return render_template('democlick.html', x_coordinate=x_coordinate, y_coordinate=y_coordinate,screen_x=screen_x, screen_y=screen_y, zoomLevel=zoomLevel)
     else:
         return redirect(url_for('index'))
 
--- helper.py
+++ helper.py
@@ -6,17 +6,23 @@ def add_data(db, form):
     from app import User
     x_coordinate = form['leftPixel']
     y_coordinate = form['topPixel']
+    screen_x = form['screenWidth']
+    screen_y = form['screenHeight']
+    zoomLevel = form['zoomLevel']
     time = datetime.datetime.now()
-    data = User(x_coordinate, y_coordinate, time)
+    data = User(x_coordinate, y_coordinate, screen_x, screen_y, zoomLevel, time)
     db.session.add(data)
     db.session.commit()
     placeholder = None
-    return x_coordinate,y_coordinate, placeholder
+    return x_coordinate,y_coordinate, screen_x, screen_y, zoomLevel, placeholder
 
 def fetch_data(db, placeholder):
     from app import User
     x_coord = []
     y_coord = []
+    screen_xs = []
+    screen_ys = []
+    zoomLevels = []
     users = db.session.query(User)
     length = 0
     for user in users:
@@ -30,10 +36,13 @@ def fetch_data(db, placeholder):
             try:
                 x_coord.append(int(float(user.x_coordinate)))
                 y_coord.append(int(float(user.y_coordinate)))
+                screen_xs.append(int(float(user.screen_x)))
+                screen_ys.append(int(float(user.screen_y)))
+                zoomLevels.append(int(float(user.zoomLevel)))
                 length+=1
             except ValueError:
                 #delete entry
                 db.session.delete(user)
                 db.session.commit()
     length = int(length)
-    return x_coord, y_coord, length
+    return x_coord, y_coord, screen_xs, screen_ys, zoomLevels, length
